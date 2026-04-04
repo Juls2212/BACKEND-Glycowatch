0fa85bd37d5492d4a8b85e0d2308e3cfeed237b6
Juls <juliana.penafiel@campusucc.edu.co>
2026-04-03 19:55:19 -0500

    remove local run script from repository tracking

--- run-local.ps1
+++
@@ -1,9 +0,0 @@
-$env:DB_HOST="localhost"
-$env:DB_PORT="5432"
-$env:DB_NAME="glycowatch"
-$env:DB_USER="postgres"
-$env:DB_PASSWORD="22Diciembre2006"
-$env:DB_SSL_MODE="disable"
-$env:JWT_SECRET="glycowatch-super-secret-key-123456789"
-
-mvn spring-boot:run
