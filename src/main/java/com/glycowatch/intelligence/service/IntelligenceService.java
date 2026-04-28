0001f0585a787f97a04c4886fe4810b07da6f243
Juls <juliana.penafiel@campusucc.edu.co>
2026-04-27 22:57:31 -0500

    implementation of intelligent service recovery logic

--- src/main/java/com/glycowatch/intelligence/service/IntelligenceService.java
+++ src/main/java/com/glycowatch/intelligence/service/IntelligenceService.java
@@ -1,8 +1,12 @@
 package com.glycowatch.intelligence.service;
 
+import com.glycowatch.intelligence.dto.IntelligenceHistoryItemResponse;
 import com.glycowatch.intelligence.dto.IntelligenceSummaryResponse;
+import java.util.List;
 
 public interface IntelligenceService {
 
     IntelligenceSummaryResponse getSummary(String authenticatedEmail);
+
+    List<IntelligenceHistoryItemResponse> getHistory(String authenticatedEmail);
 }
