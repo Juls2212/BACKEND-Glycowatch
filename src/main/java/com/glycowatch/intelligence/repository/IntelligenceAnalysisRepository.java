bb7834d9418aa8741962c4f5ad8cb5d97b0d0063
Juls <juliana.penafiel@campusucc.edu.co>
2026-05-13 11:01:29 -0500

    Intelligent analysis configuration

--- src/main/java/com/glycowatch/intelligence/repository/IntelligenceAnalysisRepository.java
+++ src/main/java/com/glycowatch/intelligence/repository/IntelligenceAnalysisRepository.java
@@ -2,6 +2,7 @@ package com.glycowatch.intelligence.repository;
 
 import com.glycowatch.intelligence.model.IntelligenceAnalysis;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface IntelligenceAnalysisRepository extends JpaRepository<IntelligenceAnalysis, Long> {
@@ -9,4 +10,6 @@ public interface IntelligenceAnalysisRepository extends JpaRepository<Intelligen
     List<IntelligenceAnalysis> findByUserIdOrderByCreatedAtDesc(Long userId);
 
     List<IntelligenceAnalysis> findTop20ByUserIdOrderByCreatedAtDesc(Long userId);
+
+    Optional<IntelligenceAnalysis> findFirstByUserIdOrderByCreatedAtDesc(Long userId);
 }
